123188532025f4df2121cee833a8984135b47e01
homeschool38 <info@mymuslimhomeschool.com>
2026-06-18 14:09:49 -0400

    Add square-config.php for configuration settings

---
+++ api/api/square-config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'environment' => 'production',
+  'access_token' => 'PASTE_TOKEN_IN_HOSTINGER_ONLY',
+  'location_id' => 'LKW1A7STZJV5T',
+  'application_id' => 'sq0idp-FXfoiRzDvk6Cj2dzc9RibA',
+];
